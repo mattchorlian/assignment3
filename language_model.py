6cd45ac2d83eccb7b702a185e752dda92cb47c78
Matthew Chorlian <mattchorlian@berkeley.edu>
2021-03-26 10:13:07 -0700

    pushing language model

--- language_model.py
+++ language_model.py
@@ -11,22 +11,22 @@ class LanguageModel(nn.Module):
         # Create an embedding layer of shape [vocab_size, rnn_size]
         # Use nn.Embedding
         # That will map each word in our vocab into a vector of rnn_size size.
-        self.embedding = your_code
+        self.embedding = nn.Embedding(vocab_size, rnn_size)
 
         # Create an LSTM layer of rnn_size size. Use any features you wish.
         # We will be using batch_first convention
-        self.lstm = your_code
+        self.lstm = nn.LSTM(rnn_size, 256, num_layers)
         # LSTM layer does not add dropout to the last hidden output.
         # Add this if you wish.
-#         self.dropout = your_code
+        self.dropout = nn.Dropout(p dropout)
         # Use a dense layer to project the outputs of the RNN cell into logits of
         # the size of vocabulary (vocab_size).
-        self.output =       
+        self.output = nn.linear(rnn_size, vocab_size)
         
     def forward(self,x):
-        embeds = your_code
-        lstm_out, _ = your_code
-#         lstm_drop = your_code
+        embeds = self.embedding(x)
+        lstm_out, _ = self.lstm(embeds)
+        lstm_drop = self.dropout(lstm_out)
 #         logits = your_code
-        logits = your_code
+        logits = self.output(lstm_drop)
         return logits
